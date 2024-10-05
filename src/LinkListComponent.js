bbc1a12b75c564ebe5fe6e7a79fdcbbab82bc40b
LathanDevers <maxime.daniels@outlook.com>
2024-10-06 00:57:17 +0200

    Test Docker

--- src/LinkListComponent.js
+++ src/LinkListComponent.js
@@ -9,15 +9,13 @@ function LinkListComponent({titles,links,summaries}) {
                 {links.map((link,index) => <LinkComponent title={titles[index]} link={link} summary={summaries[index]}></LinkComponent>)}
             </ul>
             <div id="loader-links"></div>
-            <div id="loader-list" className="flex-col animate-pulse hidden pl-5 pr-5 pt-5 pb-5">
-                <div className="">
-                    <div className="flex h-4 bg-gray-200 rounded-full dark:bg-gray-700 mb-4 w-1/3"></div>
-                    <div className="flex h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-4 w-5/6"></div>
-                    <div className="flex h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-4 w-3/4"></div>
-                    <div className="flex h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-4 w-4/5"></div>
-                    <div className="flex h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-4 w-3/4"></div>
-                    <div className="flex h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-4 w-2/3"></div>
-                </div>
+            <div id="loader-list" className="flex flex-col animate-pulse pl-5 pr-5 pt-5 pb-5">
+                <div className="flex h-4 bg-gray-200 rounded-full dark:bg-gray-700 mb-4 w-1/3"></div>
+                <div className="flex h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-4 w-5/6"></div>
+                <div className="flex h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-4 w-3/4"></div>
+                <div className="flex h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-4 w-4/5"></div>
+                <div className="flex h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-4 w-3/4"></div>
+                <div className="flex h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-4 w-2/3"></div>
             </div>
         </div>
     );
