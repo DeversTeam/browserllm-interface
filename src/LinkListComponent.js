0899ebaab45a6aae8a6d94dbc344ae5c4ea39890
LathanDevers <maxime.daniels@outlook.com>
2024-10-06 15:07:58 +0200

    Resolve bug loading links

--- src/LinkListComponent.js
+++ src/LinkListComponent.js
@@ -4,12 +4,12 @@ import LinkComponent from "./LinkComponent";
 function LinkListComponent({titles,links,summaries}) {
 
     return(
-        <div className="">
+        <div className="flex flex-col w-full ">
             <ul id="link-list" className="flex flex-col pl-5 pr-5 pb-5">
                 {links.map((link,index) => <LinkComponent title={titles[index]} link={link} summary={summaries[index]}></LinkComponent>)}
             </ul>
             <div id="loader-links"></div>
-            <div id="loader-list" className="flex flex-col animate-pulse pl-5 pr-5 pt-5 pb-5">
+            <div id="loader-list" className="flex flex-col w-full animate-pulse pl-5 pr-5 pt-5 pb-5">
                 <div className="flex h-4 bg-gray-200 rounded-full dark:bg-gray-700 mb-4 w-1/3"></div>
                 <div className="flex h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-4 w-5/6"></div>
                 <div className="flex h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-4 w-3/4"></div>
