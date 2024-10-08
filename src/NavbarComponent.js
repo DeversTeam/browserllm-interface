87fe479c65f753e1b527c1c3adf17f2668055b6d
LathanDevers <maxime.daniels@outlook.com>
2024-10-08 09:58:50 +0200

    resolve bugs navbar

--- src/NavbarComponent.js
+++ src/NavbarComponent.js
@@ -1,6 +1,6 @@
 function NavbarComponent({children}){
     return(
-        <div className="flex flex-row">
+        <div id="navbar" className="flex flex-row">
             <div className="px-7 py-5">
                 <a className="hover:underline" href="https://github.com/DeversTeam/browserllm-interface">À propos</a>
             </div>
